eec9148df3a5531f557755cd76c3ba2a6424d8c0
rkamatham1 <kamathamrakesh79@gmail.com>
2025-02-24 12:14:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # Print the current working directory to verify Flask's location
+    print("Current working directory:", os.getcwd())
     return render_template('index.html')
 
 if __name__ == '__main__':
